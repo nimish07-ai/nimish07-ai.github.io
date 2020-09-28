7a7f0fabdcbd7464cedb6e3714715f7459adb5e5
nimish07-ai <60012932+nimish07-ai@users.noreply.github.com>
2020-09-29 00:12:12 +0530

    Add files via upload

---
+++ rehanbot/how_to_use.py
@@ -0,0 +1,31 @@
+from rehanbot.bot import *
+
+seven=seven_username_checker()
+
+#to add user
+# return true if registed else fALSE
+Check=seven.Seven_add_new("seven")
+print(Check)
+#check liat.txt
+
+
+## will send false if u will add same user again
+
+
+
+# #to check if user present
+Check=seven.Seven_add_new("Seven")
+print(Check)
+#check liat.txt
+
+
+
+
+#to reset whole list
+import time
+
+time.sleep(20)
+seven.reset()
+
+
+
